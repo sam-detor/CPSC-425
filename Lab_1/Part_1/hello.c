1f0400bb90705fce0f9bdd1ef6590b34b6474bd6
sam_detor <sdetor2@gmail.com>
2022-02-09 13:24:08 -0500

    clean-up before submitting

--- Lab_1/Part_1/hello.c
+++ Lab_1/Part_1/hello.c
@@ -4,17 +4,17 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sam Detor");
-MODULE_DESCRIPTION("Printing out hello world on start-up and goodbye world on finishing. Source: Robert Oliver\'s blog post on building a basic kernel module: https://blog.sourcerer.io/writing-a-simple-linux-kernel-module-d9dc3762c234");
+MODULE_DESCRIPTION("This module prints out hello world on start-up and goodbye world on finishing. See lab report for sources");
 MODULE_VERSION("1.0");
 
 
-static int __init helloWord(void)
+static int __init helloWord(void) //This function runs when the module is loaded into the kernel
 {
     printk(KERN_INFO "Hello, World!\n");
     return 0;
 }
 
-static void __exit goodbyeWorld(void)
+static void __exit goodbyeWorld(void) //This function runs when the module is removed from the kernel
 {
     printk(KERN_INFO "Goodbye, World!\n");
 }
--- Lab_1/Part_2/hello.c
+++ Lab_1/Part_2/hello.c
@@ -5,43 +5,45 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sam Detor");
-MODULE_DESCRIPTION("Printing out hello world on start-up and goodbye world on finishing. Source: Robert Oliver\'s blog post on building a basic kernel module: https://blog.sourcerer.io/writing-a-simple-linux-kernel-module-d9dc3762c234");
+MODULE_DESCRIPTION("This module prints out hello world on start-up and goodbye world on finishing depending on the state of the"
+                    "enable_logging parameter. It also doubles the parameter double_me when it is changed by the user. See lab"
+                    "for sources.");
+                    
 MODULE_VERSION("1.0");
 
-static int enableLogging = 1;
-module_param(enableLogging, int, S_IRUGO | S_IWUSR);
+static int enable_logging = 1;
+module_param(enable_logging, int, S_IRUGO | S_IWUSR); //creates the enable logging module parameter with user read/write permissions
 
-static int doubleMe = 0;
+static int double_me = 0;
 
 
-static int double_val(const char* val, const struct kernel_param *kp)
-{
+static int double_val(const char* val, const struct kernel_param *kp) //the method that runs to write user input to the double_me module param
+{                                                                     
     int ret = param_set_int(val, kp);
     if(ret == 0)
     {
-        int oldVal = doubleMe;
-        doubleMe *= 2;
-        if(enableLogging)
+        int oldVal = double_me;
+        double_me *= 2;
+        if(enable_logging)
         {
-            printk(KERN_INFO "Initial Value: %d Doubled Value: %d \n", oldVal, doubleMe);
+            printk(KERN_INFO "Initial Value: %d Doubled Value: %d \n", oldVal, double_me);
         }
 
     }
-    printk(KERN_INFO "bad");
     return EINVAL;
 }
 
-const struct kernel_param_ops doubleMe_ops = 
+const struct kernel_param_ops double_me_ops = //the methods that run when the double_me param is set or requested
 {
     .set = &double_val,
     .get = param_get_int,
 };
 
-module_param_cb(doubleMe, &doubleMe_ops, &doubleMe, S_IRUSR | S_IWUSR);
-
-static int __init helloWord(void)
+module_param_cb(double_me, &double_me_ops, &double_me, S_IRUSR | S_IWUSR); //creates the double_me module parameter that notifies the
+                                                                           //module when the value has been changed
+static int __init helloWord(void) //the initialization method that runs when the module is loaded into the kernel
 {
-    if(enableLogging)
+    if(enable_logging)
     {
         printk(KERN_INFO "Hello, World!\n");
     }
@@ -49,9 +51,9 @@ static int __init helloWord(void)
     return 0;
 }
 
-static void __exit goodbyeWorld(void)
+static void __exit goodbyeWorld(void) //the method that runs when the module is removed from the kernel.
 {
-    if(enableLogging)
+    if(enable_logging)
     {
         printk(KERN_INFO "Goodbye, World!\n");
     }
