c07ecfdac91b225a0b227e853fa4c8ef16fe14ca
sam-detor <sdetor2@gmail.com>
2022-02-18 14:16:43 -0500

    compiled version of prelim part 3

--- Lab_1/Part_3/FileOps.c
+++ Lab_1/Part_3/FileOps.c
@@ -1,42 +1,56 @@
 #include "MemManager.h"
 
 
-
+/*
 local_llseek,
 local_read,
 local_write,
 local_ioctl,
 local_open,
 local_close,
+*/
 
-int local_open (struct inode *inode, struct file *flip)
+static int nextID = 0;
+
+int local_open (struct inode *inode, struct file *filp)
 {
-    memManagerStructure* dev;
-    dev = container_of(inode->i_cdev, memManagerStructure, cdev);
-    flip->private_data = dev;
+    struct myMem_struct* dev = container_of(inode->i_cdev, struct myMem_struct, my_cdev);
+    filp->private_data = dev;
     return 0;
 }
 
-int local_close(struct inode* inode, struct file flip)
+int local_close(struct inode* inode, struct file filp)
 {
-    // also has to deallocate what was allocated in flip->private data
+    // also has to deallocate what was allocated in filp->private data
     return 0;
 }
 
-ssize_t local_read (struct file* flip, const char _ _user *buff, size_t count, loff_t *offp)
+ssize_t local_read (struct file* filp, char __user *buff, size_t count, loff_t *offp)
 {
+    struct myMem_struct* dev = (filp->private_data);
+    struct region* data_region = dev->current_region;
+    char byteToRead;
+    int ret;
+
     if(count != 1)
     {
-        return -EINVAL
+        return -EINVAL;
     }
-    struct region* data_region = (flip->private_data)->current_region;
+
+
+    if(data_region == NULL)
+    {
+        return 0;
+    }
+
     if(*offp >= data_region->region_size)
     {
         return 0;
     }
 
-    char byteToRead = (data_region->data)[data_region->offset];
-    int ret = copy_to_user(buf,&byteToRead,count);
+    
+    byteToRead = (data_region->data)[data_region->offset];
+    ret = copy_to_user(buff,&byteToRead,count);
     if(ret == 0)
     {
         return -EFAULT;
@@ -47,20 +61,28 @@ ssize_t local_read (struct file* flip, const char _ _user *buff, size_t count, l
     return count;
 }
 
-ssize_t local_write (struct file* flip, const char _ _user *buff, size_t count, loff_t *offp)
+ssize_t local_write (struct file* filp, const char __user *buff, size_t count, loff_t *offp)
 {
+
+    struct region* data_region = ((struct myMem_struct*)(filp->private_data))->current_region;
+    int ret;
+
     if(count != 1)
     {
-        return -EINVAL
+        return -EINVAL;
     }
 
-    struct region* data_region = (flip->private_data)->current_region;
+    if(data_region == NULL)
+    {
+        return 0;
+    }
+
     if(*offp >= data_region->region_size)
     {
         return 0;
     }
 
-    int ret = copy_from_user((data_region->data)[data_region->offset], buff, count);
+    ret = copy_from_user(&((data_region->data)[data_region->offset]), buff, count);
     if(ret == 0)
     {
         return -EFAULT;
@@ -72,9 +94,9 @@ ssize_t local_write (struct file* flip, const char _ _user *buff, size_t count, 
     
 }
 
-local_llseek(struct file * flip, loff_t off, int whence)
+loff_t local_llseek(struct file * filp, loff_t off, int whence)
 {
-    struct region* data_region = (flip->private_data)->current_region;
+    struct region* data_region = ((struct myMem_struct*)(filp->private_data))->current_region;
     loff_t newPos;
 
     switch(whence)
@@ -85,10 +107,11 @@ local_llseek(struct file * flip, loff_t off, int whence)
 
         case 1:
         newPos = data_region->offset + off;
-        break
+        break;
 
         case 2:
         newPos = data_region->region_size + off;
+        break;
 
         default:
         return -EINVAL;
@@ -96,31 +119,99 @@ local_llseek(struct file * flip, loff_t off, int whence)
 
     if(newPos > 0 && newPos <= data_region->region_size)
     {
-        flip->f_pos = newPos;
+        filp->f_pos = newPos;
         data_region->offset = newPos;
         return newPos;
 
     }
     else
     {
-        return -EINVAL
+        return -EINVAL;
     }
 }
 
-int local_ioctl(struct file* filp, unisgned int cmd, unsigned long arg)
+int local_ioctl(struct file* filp, unsigned int cmd, unsigned long arg)
 {
+    struct myMem_struct* dev = filp->private_data;
+    struct region* head = dev->data_region;
+    struct region* new_region;
+    char* allocated_data;
+    struct region* temp;
+    struct region* temp_prev;
+    int regionNum;
     switch(cmd)
     {
         case MYMEM_IOCTL_ALLOC:
-        struct region* new_region = kmalloc(sizeof(struct region), GFP_KERNEL);
+        if(dev->bytes_allocated + (unsigned int)arg > MAX_MEM)
+        {
+            return -ENOMEM;
+        }
+
+        new_region = kmalloc(sizeof(struct region), GFP_KERNEL);
+        if(new_region == NULL)
+        {
+            return -ENOMEM;
+        }
+        allocated_data = kmalloc(arg, GFP_KERNEL);
+        if(allocated_data == NULL)
+        {
+            return -ENOMEM;
+        }
+
+        temp = head;
         
+        if(dev->data_region == NULL)
+        {
+            dev->data_region = new_region;
+            dev->current_region = new_region;
+            dev->current_region_number = nextID;
+            new_region->region_number = nextID;
+            nextID++;
+            new_region->next = NULL;
+            new_region->offset = 0;
+            new_region->data = allocated_data;
+            new_region->region_size = (unsigned int)arg;
+            dev->bytes_allocated = (unsigned int)arg;
+            return 0;
+        }
+        
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_region;
+        new_region->region_number = nextID;
+        nextID++;
+        new_region->next = NULL;
+        new_region->offset = 0;
+        new_region->data = allocated_data;
+        new_region->region_size = (unsigned int)arg;
+        dev->bytes_allocated += (unsigned int)arg;
+        return new_region->region_number;
         break;
 
         case MYMEM_IOCTL_FREE:
+        
+        temp = head;
+        regionNum = (int)arg;
+        while(temp != NULL && temp->region_number != regionNum)
+        {
+            temp_prev = temp;
+            temp = temp->next;
+        }
+        if(temp == NULL)
+        {
+            return -EINVAL;
+        }
+        temp_prev->next = temp->next;
+        dev->bytes_allocated -= temp->region_size;
+        kfree(temp->data); 
+        return 0;
         break;
         
         case MYMEM_IOCTL_SETREGION:
-        break:
+        return -ENOTTY;
+        break;
 
         default:
         return -ENOTTY;
--- Lab_1/Part_3/MemManager.h
+++ Lab_1/Part_3/MemManager.h
@@ -5,30 +5,34 @@
 #include <linux/fs.h>
 #include <linux/device.h>
 #include <linux/kdev_t.h>
-#include <asm/uacess.h>
+#include <linux/uaccess.h>
 #include <linux/ioctl.h>
+#include <linux/cdev.h>
+#include <linux/slab.h>
 
 #define MYMEM_IOCTL_ALLOC (0)
 #define MYMEM_IOCTL_FREE (1)
 #define MYMEM_IOCTL_SETREGION (2)
+#define MAX_MEM (1048576)
 
 struct region
 {
-    char* data,
-    int region_size,
-    int region_number,
-    int offset
-} 
+    char* data;
+    unsigned int region_size;
+    int region_number;
+    int offset;
+    struct region* next;
+};
 
 struct myMem_struct
 {
-    struct region* current_region,
-    struct region* data_region,
-    int current_region_number,
-    int bytes_allocated,
-    struct cdev my_cdev
+    struct region* current_region;
+    struct region* data_region;
+    int current_region_number;
+    unsigned int bytes_allocated;
+    struct cdev my_cdev;
 
-} memManagerStructure;
+};
 
 
 int local_open (struct inode *inode, struct file *flip);
