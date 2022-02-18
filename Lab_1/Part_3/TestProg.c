f61a474c058eb1510cba4404c91c2270f73a562c
sam-detor <sdetor2@gmail.com>
2022-02-18 14:47:23 -0500

    fixed test prog

--- Lab_1/Part_3/TestProg.c
+++ Lab_1/Part_3/TestProg.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-#include <unistad.h>
-#include <sys/sycall.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <errno.h>
+#include <fcntl.h>
 
 int main()
 {
-    int fd = open("/dev/mymem", O_RDWR);
+    int fd = syscall(SYS_open, "/dev/mymem", O_RDWR);
     printf("file descriptor %d", fd);
-    int ret = close(fd);
+    int ret = syscall(SYS_close, fd);
     printf("close return val: %d", ret);
 }
