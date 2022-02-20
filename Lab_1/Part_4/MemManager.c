43a97153e824377bce9a04f4438837dac02c7fda
sam-detor <sdetor2@gmail.com>
2022-02-20 15:35:26 -0500

    added source files from pt3 to get pt4 to compile

---
+++ Lab_1/Part_4/MemManager.c
@@ -0,0 +1,95 @@
+#include "MemManager.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Sam Detor");
+MODULE_DESCRIPTION("1 byte memory manager. See lab report for sources");
+                    
+MODULE_VERSION("1.0");
+
+module_param(param_bytes_allocated, int, S_IRUGO);
+
+char* deviceName = "mymem";
+dev_t devNums;
+unsigned int count = 1;
+struct kobject *kobj_ref;
+struct kobj_attribute mymem_attribute = __ATTR(dataRegions,S_IRUGO, sysfs_show, sysfs_store);
+
+struct file_operations memManager_fops = 
+{
+    .owner = THIS_MODULE,
+    .llseek = local_llseek,
+    .read = local_read,
+    .write = local_write,
+    .unlocked_ioctl = local_ioctl,
+    .open = local_open,
+    .release = local_close,
+};
+
+struct myMem_struct mymem;
+
+static struct class *myClass;
+static struct device *myDev;
+
+
+
+
+static int __init memManagerInit(void) //the initialization method that runs when the module is loaded into the kernel
+{    
+    int ret = alloc_chrdev_region(&devNums, 0, count, deviceName);
+    if(ret < 0)
+    {
+        printk(KERN_INFO "unable to allocate region");
+        return ret;
+    }
+
+    cdev_init(&(mymem.my_cdev), &memManager_fops);
+    mymem.my_cdev.ops = &memManager_fops;
+    mymem.my_cdev.owner = THIS_MODULE;
+    ret = cdev_add(&(mymem.my_cdev), devNums, count);
+    if(ret < 0)
+    {
+        printk(KERN_INFO "unable to create the add cdev");
+        unregister_chrdev_region(devNums, count);
+        return ret;
+    }
+
+    myClass = class_create(THIS_MODULE,"mymem_class");
+    if(myClass == NULL)
+    {
+        printk(KERN_INFO "unable to create the class");
+        cdev_del(&(mymem.my_cdev));
+        unregister_chrdev_region(devNums, count);
+        return -1;
+    }
+    myDev = device_create(myClass, NULL, devNums, NULL, "mymem");
+    if(myDev == NULL)
+    {
+        printk(KERN_INFO "unable to create device");
+        class_destroy(myClass);
+        cdev_del(&(mymem.my_cdev));
+        unregister_chrdev_region(devNums, count);
+        return -1;
+    }
+
+    kobj_ref = kobject_create_and_add("regions", kernel_kobj);
+    if(sysfs_create_file(kobj_ref, &mymem_attribute.attr))
+    {
+        kobject_put(kobj_ref);
+        sysfs_remove_file(kernel_kobj, &mymem_attribute.attr);
+    }
+
+    return 0;
+}
+
+static void __exit memManagerExit(void) //the method that runs when the module is removed from the kernel.
+{
+    kobject_put(kobj_ref);
+    sysfs_remove_file(kernel_kobj, &mymem_attribute.attr);
+    cdev_del(&(mymem.my_cdev));
+    device_destroy(myClass,devNums);
+    class_destroy(myClass);
+    unregister_chrdev_region(devNums,count);
+}
+
+module_init(memManagerInit);
+module_exit(memManagerExit);
---
+++ Lab_1/Part_4/MemManager.h
@@ -0,0 +1,48 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/kdev_t.h>
+#include <linux/uaccess.h>
+#include <linux/ioctl.h>
+#include <linux/cdev.h>
+#include <linux/slab.h>
+#include <linux/sysfs.h>
+#include <linux/kobject.h>
+#include <linux/kdev_t.h>
+
+#define MAX_MEM (1048576)
+
+extern int param_bytes_allocated;
+extern struct region* dataRegions;
+
+struct region
+{
+    char* data;
+    unsigned int region_size;
+    int region_number;
+    int offset;
+    struct region* next;
+};
+
+struct myMem_struct
+{
+    struct region* current_region;
+    struct region* data_region;
+    int current_region_number;
+    unsigned int bytes_allocated;
+    struct cdev my_cdev;
+
+};
+
+
+int local_open (struct inode *inode, struct file *flip);
+int local_close(struct inode* inode, struct file *filp);
+ssize_t local_read (struct file* filp, char __user *buff, size_t count, loff_t *offp);
+ssize_t local_write (struct file* filp, const char __user *buff, size_t count, loff_t *offp);
+loff_t local_llseek(struct file * filp, loff_t off, int whence);
+long int local_ioctl(struct file* filp, unsigned int cmd, unsigned long arg);
+ssize_t sysfs_show(struct kobject *kobj, struct kobj_attribute * attr, char* buf);
+ssize_t sysfs_store(struct kobject *kobj, struct kobj_attribute * attr, const char* buf, size_t count);
