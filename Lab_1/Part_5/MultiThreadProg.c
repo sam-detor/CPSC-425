db2c2bd82e18a409b0684fd9ffc64481eb9f6f4a
sam-detor <sdetor2@gmail.com>
2022-02-20 19:08:40 -0500

    fixed some prelim issues

--- Lab_1/Part_5/MultiThreadProg.c
+++ Lab_1/Part_5/MultiThreadProg.c
@@ -7,6 +7,13 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <sys/wait.h>
+
+#define MYMEM_IOCTL_ALLOC _IOW(236,0,int*)
+#define MYMEM_IOCTL_FREE _IOW(236,1,int*)
+#define MYMEM_IOCTL_SETREGION _IOW(236,2,int*)
+
 int main()
 {
     pid_t child_pid, wpid;
@@ -17,7 +24,7 @@ int main()
     //Parent Code
     int fd = open("/dev/mymem_smart", O_RDWR);
     int num1 = ioctl(fd,MYMEM_IOCTL_ALLOC,&size);
-    uint64_t myCounter = 0;
+    unsigned long long myCounter = 0;
     if(write(fd,&myCounter, 8) != 8)
     {
         printf("couldn't write");
@@ -27,7 +34,7 @@ int main()
 
     for (int id=0; id<n; id++) {
         if ((child_pid = fork()) == 0) {
-            uint64_t childCounter;
+            unsigned long long childCounter;
             lseek(fd, 0, 0);
             read(fd, &childCounter, 8);
             childCounter++;
@@ -39,4 +46,7 @@ int main()
     }
 
     while ((wpid = wait(&status)) > 0);
+    lseek(fd, 0, 0);
+    read(fd, &myCounter, 8);
+    printf("Counter Val: %lld", myCounter);
 }
