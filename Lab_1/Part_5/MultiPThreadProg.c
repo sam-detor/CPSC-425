a640cd93fdf24beb965468caf2319a42c8f4fdcb
sam-detor <sdetor2@gmail.com>
2022-02-20 20:48:05 -0500

    finished coding part of pt_5

--- Lab_1/Part_5/MultiPThreadProg.c
+++ Lab_1/Part_5/MultiPThreadProg.c
@@ -15,20 +15,40 @@
 #define MYMEM_IOCTL_FREE _IOW(236,1,int*)
 #define MYMEM_IOCTL_SETREGION _IOW(236,2,int*)
 
+#define WORKERS (400)
+#define N (100000)
+
 pthread_mutex_t lock;
+pthread_t threads[WORKERS];
+int fd;
+
+void *forThread(void * ptr)
+{
+    unsigned long long childCounter;
+    for(int j = 0; j < N; j++)
+    {
+        pthread_mutex_lock(&lock);
+        lseek(fd, 0, 0);
+        read(fd, &childCounter, 8);
+        childCounter++;
+        lseek(fd, 0, 0);
+        write(fd, &childCounter, 8);
+        pthread_mutex_unlock(&lock);
+                
+    }
+    pthread_exit(0);
+    return (void *)0;
+}
 
 int main()
 {
-    pid_t child_pid, wpid;
-    int status = 0;
     int size = 10;
-    int workers = 40;
-    int n = 5;
 
     //Parent Code
-    int fd = open("/dev/mymem_smart", O_RDWR);
+    fd = open("/dev/mymem_smart", O_RDWR);
     int num1 = ioctl(fd,MYMEM_IOCTL_ALLOC,&size);
     unsigned long long myCounter = 0;
+
     if(write(fd,&myCounter, 8) != 8)
     {
         printf("couldn't write");
@@ -41,28 +61,18 @@ int main()
         return 1;
     }
 
-    for (int id=0; id < workers; id++) {
-        if ((child_pid = fork()) == 0) {
-            unsigned long long childCounter;
-            for(int j = 0; j < n; j++)
-            {
-                pthread_mutex_lock(&lock);
-                lseek(fd, 0, 0);
-                read(fd, &childCounter, 8);
-                childCounter++;
-                lseek(fd, 0, 0);
-                write(fd, &childCounter, 8);
-                pthread_mutex_unlock(&lock);
-                
-            }
-            exit(0);
-        }
+    for (int id=0; id < WORKERS; id++) {
+       pthread_create(&threads[id],NULL, forThread, NULL);
     }
 
-    while ((wpid = wait(&status)) > 0);
+    for (int id=0; id < WORKERS; id++) {
+       pthread_join(threads[id],NULL);
+    }
     lseek(fd, 0, 0);
     read(fd, &myCounter, 8);
     close(fd);
     pthread_mutex_destroy(&lock);
     printf("Counter Val: %lld\n", myCounter);
 }
+
+
---
+++ Lab_1/Part_5/UnsafeMultiPThreadProg.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/wait.h>
+#include <pthread.h>
+
+#define MYMEM_IOCTL_ALLOC _IOW(236,0,int*)
+#define MYMEM_IOCTL_FREE _IOW(236,1,int*)
+#define MYMEM_IOCTL_SETREGION _IOW(236,2,int*)
+
+#define WORKERS (4)
+#define N (1000)
+
+pthread_t threads[WORKERS];
+int fd;
+
+void *forThread(void * ptr)
+{
+    unsigned long long childCounter;
+    for(int j = 0; j < N; j++)
+    {
+        //If the call fails, it retries it, as per the ed discussion post
+        if(lseek(fd, 0, 0) < 0);
+        {
+            lseek(fd, 0, 0);
+        }
+        if(read(fd, &childCounter, 8) < 8)
+        {
+            read(fd, &childCounter, 8);
+        }
+        childCounter++;
+        if(lseek(fd, 0, 0) < 0);
+        {
+            lseek(fd, 0, 0);
+        }
+        if(write(fd, &childCounter, 8) < 8)
+        {
+            write(fd, &childCounter, 8);
+        }
+                
+    }
+    pthread_exit(0);
+    return (void *)0;
+}
+
+int main()
+{
+    int size = 10;
+
+    //Parent Code
+    fd = open("/dev/mymem_smart", O_RDWR);
+    int num1 = ioctl(fd,MYMEM_IOCTL_ALLOC,&size);
+    unsigned long long myCounter = 0;
+
+    if(write(fd,&myCounter, 8) != 8)
+    {
+        printf("couldn't write");
+        return 0;
+    }
+    lseek(fd,0,0);
+
+
+    for (int id=0; id < WORKERS; id++) {
+       pthread_create(&threads[id],NULL, forThread, NULL);
+    }
+
+    for (int id=0; id < WORKERS; id++) {
+       pthread_join(threads[id],NULL);
+    }
+    lseek(fd, 0, 0);
+    read(fd, &myCounter, 8);
+    close(fd);
+    printf("Counter Val: %lld\n", myCounter);
+}
+
+
