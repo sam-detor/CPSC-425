bbed2c412cc058f6a1572c4041c3cab2d478ed71
sam-detor <sdetor2@gmail.com>
2022-02-21 11:28:46 -0500

    made some quick changes to multi thread prog

--- Lab_1/Part_5/UnsafeMultiPThreadProg.c
+++ Lab_1/Part_5/UnsafeMultiPThreadProg.c
@@ -15,16 +15,18 @@
 #define MYMEM_IOCTL_FREE _IOW(236,1,int*)
 #define MYMEM_IOCTL_SETREGION _IOW(236,2,int*)
 
-#define WORKERS (4)
-#define N (1000)
+#define WORKERS (2)
+#define N (10)
 
 pthread_t threads[WORKERS];
 int fd;
+int n = N;
+int workers = WORKERS;
 
 void *forThread(void * ptr)
 {
     unsigned long long childCounter;
-    for(int j = 0; j < N; j++)
+    for(int j = 0; j < n; j++)
     {
         //If the call fails, it retries it, as per the ed discussion post
         if(lseek(fd, 0, 0) < 0);
@@ -46,7 +48,6 @@ void *forThread(void * ptr)
         }
                 
     }
-    pthread_exit(0);
     return (void *)0;
 }
 
@@ -67,17 +68,17 @@ int main()
     lseek(fd,0,0);
 
 
-    for (int id=0; id < WORKERS; id++) {
+    for (int id=0; id < workers; id++) {
        pthread_create(&threads[id],NULL, forThread, NULL);
     }
 
-    for (int id=0; id < WORKERS; id++) {
+    for (int id=0; id < workers; id++) {
        pthread_join(threads[id],NULL);
     }
     lseek(fd, 0, 0);
     read(fd, &myCounter, 8);
     close(fd);
-    printf("Counter Val: %lld\n", myCounter);
+    printf("N: %d, W: %d, Counter Val: %lld\n", N, WORKERS, myCounter);
 }
 
 
