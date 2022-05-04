5534758d2089c5515719cc17fa6c8addb245ceac
sam-detor <sdetor2@gmail.com>
2022-05-04 17:49:01 -0400

    updated build.rs

--- Lab_5/Part1/part1_cargo/build.rs
+++ Lab_5/Part1/part1_cargo/build.rs
@@ -1,5 +1,5 @@
 
 fn main() {
-    println!("cargo:rustc-link-search=native=../task_bin");
+    println!("cargo:rustc-link-search=native=../task-bin");
     println!("cargo:rustc-link-lib=static=flash_blue");
 }
